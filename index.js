e2dc39e5dbf65a62a72cbfa0cf8b369303950df7
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2024-08-17 00:32:49 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import expressWs from "express-ws";
+
+const app = express();
+const wss = expressWs(app);
+
+app.ws("/ws", (ws) => ws.on("message", (msg) => wss.getWss().clients.forEach((client) => client.send(msg))));
+
+app.listen(7277);
