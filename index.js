3ed8cc50d264a97d48f27f03d5fe0c992a6e7c32
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2024-08-17 01:04:20 +0700

    Remove path

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import expressWs from "express-ws";
 const app = express();
 const wss = expressWs(app);
 
-app.ws("/ws", (ws) => ws.on("message", (msg) => wss.getWss().clients.forEach((client) => client.send(msg))));
+app.ws("/", (ws) => ws.on("message", (msg) => wss.getWss().clients.forEach((client) => client.send(msg))));
 
 app.listen(7277);
