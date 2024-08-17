0498e5a6d158adb63daa7eb42dc4c1a78cd7c180
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2024-08-18 04:13:17 +0700

    Added user join noti

--- index.js
+++ index.js
@@ -9,21 +9,41 @@ let userCount = 0;
 app.ws("/", (ws) => {
     ws.on("message", (msg) => {
         // console.log(msg);
-        wss.getWss().clients.forEach((client) => client.send(msg));
+        const { type, payload } = JSON.parse(msg);
+
+        if (type === "NEW_USER") {
+            const { username } = payload;
+
+            wss.getWss().clients.forEach((client) =>
+                client.send(
+                    JSON.stringify({
+                        type: "USER_STATE",
+                        payload: {
+                            username,
+                            state: "JOIN",
+                        },
+                    })
+                )
+            );
+        }
+
+        if (type === "NEW_MESSAGE") {
+            wss.getWss().clients.forEach((client) => client.send(msg));
+        }
     });
 
     ws.on("close", () => {
         // console.log("Someone Disconnected?");
         userCount--;
 
-        wss.getWss().clients.forEach((client) =>
+        wss.getWss().clients.forEach((client) => {
             client.send(
                 JSON.stringify({
                     type: "UPDATE_USERCOUNT",
                     payload: userCount,
                 })
-            )
-        );
+            );
+        });
     });
 });
 
