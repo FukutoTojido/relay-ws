55e4fc1d10780451fe27c76862ea31babe8c57aa
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2024-08-17 14:33:39 +0700

    Update usercount

--- index.js
+++ index.js
@@ -4,11 +4,41 @@ import expressWs from "express-ws";
 const app = express();
 const wss = expressWs(app);
 
-app.ws("/", (ws) =>
+let userCount = 0;
+
+app.ws("/", (ws) => {
     ws.on("message", (msg) => {
-        console.log(msg);
+        // console.log(msg);
         wss.getWss().clients.forEach((client) => client.send(msg));
-    })
-);
+    });
+
+    ws.on("close", () => {
+        // console.log("Someone Disconnected?");
+        userCount--;
+
+        wss.getWss().clients.forEach((client) =>
+            client.send(
+                JSON.stringify({
+                    type: "UPDATE_USERCOUNT",
+                    payload: userCount,
+                })
+            )
+        );
+    });
+});
+
+wss.getWss().on("connection", () => {
+    // console.log("Someone Connected?");
+    userCount++;
+
+    wss.getWss().clients.forEach((client) =>
+        client.send(
+            JSON.stringify({
+                type: "UPDATE_USERCOUNT",
+                payload: userCount,
+            })
+        )
+    );
+});
 
 app.listen(7277);
